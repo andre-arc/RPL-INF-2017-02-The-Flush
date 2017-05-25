6cc7ec53ec10790047b740c6ee1fc0b194b3389f
andre-arc <andridarnius@gmail.com>
2017-05-25 18:46:49 +0700

    denah_interaktif v1.1

--- mailbox.php
+++ mailbox.php
@@ -523,21 +523,9 @@
                     </div>
                     <div class="sidebar-account-settings">
                         <ul>
+                            
                             <li class="no-padding">
-                                <a class="waves-effect waves-grey"><i class="material-icons">mail_outline</i>Inbox</a>
-                            </li>
-                            <li class="no-padding">
-                                <a class="waves-effect waves-grey"><i class="material-icons">star_border</i>Starred<span class="new badge">18</span></a>
-                            </li>
-                            <li class="no-padding">
-                                <a class="waves-effect waves-grey"><i class="material-icons">done</i>Sent Mail</a>
-                            </li>
-                            <li class="no-padding">
-                                <a class="waves-effect waves-grey"><i class="material-icons">history</i>History<span class="new grey lighten-1 badge">3 new</span></a>
-                            </li>
-                            <li class="divider"></li>
-                            <li class="no-padding">
-                                <a class="waves-effect waves-grey"><i class="material-icons">exit_to_app</i>Log Out</a>
+                                <a href="<?= BASE_URL.'logout.php'?>" class="waves-effect waves-grey"><i class="material-icons">exit_to_app</i>Log Out</a>
                             </li>
                         </ul>
                     </div>
